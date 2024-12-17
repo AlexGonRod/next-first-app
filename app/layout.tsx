0518559aed01fdb8975ada6b39cbc2586823c261
AlexGonRod <alexgonrod83@gmail.com>
2024-12-17 11:05:19 +0100

    adding metadate

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css'
 import { inter } from './ui/fonts';
+import { Metadata, } from 'next';
+
+export const metadata: Metadata = {
+	title: {
+		template: '%s | Acme Dashboard',
+		default: 'Acme Dashboard'
+	},
+	description: 'Learning Next js',
+	 metadataBase: new URL('https://next-app.vercel.sh'),
+}
 
 export default function RootLayout({
   children,
